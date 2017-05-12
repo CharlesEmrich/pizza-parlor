52fa391cef50334dfc658ece00c26bcf08e910ce
Charles Emrich <libertinistic@gmail.com>
2017-05-12 11:51:33 -0700

    add appending of toppings to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,73 @@
 //// Business Logic ////
+function Parlor (sizes, toppings) {
+  this.sizes    = sizes;
+  this.toppings = toppings;
+}
+
 function Pizza (size, toppings) {
   this.size     = size;
   this.toppings = [];
   this.price    = 0;
 }
-
-function Topping (name, delta, image) {
+function Topping (name, delta, id, image) {
   this.name  = name;
   this.delta = delta;
+  this.id    = id;
   this.image = image;
 }
 
-var toppings = [[],
-]
+function Size (name, delta, id) {
+  this.name  = name;
+  this.delta = delta;
+  this.id    = id;
+}
 
-var sizes = [["Small (10\") 8.99", "sm"], ["Medium (12\") 10.99","md"], ["Large (14\") 12.99", "lg"]];
+var toppings = [["Pepperoni", "2.00", "pepperoni", ""],
+                ["Sliced Italian Sausage", "2.00", "sliced-italian-sausage", ""],
+                ["Philly Steak", "2.50", "philly-steak", ""],
+                ["Bacon", "1.50", "bacon", ""],
+                ["Premium Chicken", "2.00", "premium-chicken", ""],
+                ["Ham", "2.00", "ham", ""],
+                ["Cheddar Cheese", "1.50", "cheddar-cheese", ""],
+                ["Banana Peppers", "1.00", "banana-peppers", ""],
+                ["Mushrooms", "1.00", "mushrooms", ""],
+                ["Onions", "0.50", "onions", ""],
+                ["Spinach", "1.00", "spinach", ""],
+                ["Black Olives", "1.00", "black-olives", ""],
+                ["Diced Tomatoes", "0.50", "diced-tomatoes", ""]
+];
+var sizes = [["Small (10\")", 8.99, "sm"], ["Medium (12\")", 10.99,"md"], ["Large (14\")", 12.99, "lg"]];
+var ourParlor = new Parlor([], []);
+
+for (var i = 0; i < toppings.length; i++) {
+  var newTopping = new Topping (toppings[i][0], toppings[i][1], toppings[i][2], toppings[i][3]);
+  ourParlor.toppings.push(newTopping);
+}
+for (var i = 0; i < sizes.length; i++) {
+  var newSize = new Size (sizes[i][0], sizes[i][1], sizes[i][2]);
+  ourParlor.sizes.push(newSize);
+}
+
 //// User Interface Logic ////
 $(function() {
-  for (var i = 0; i < sizes.length; i++) {
+  console.log(ourParlor);
+  //BUG: For some reason, this creates radio boxes that can all be selected.
+  for (var i = 0; i < ourParlor.sizes.length; i++) {
     $("#sizes").append(
       "<div class='radio'>" +
         "<label>" +
-          "<input type='radio' value='" + sizes[i][1] + "'>" +
-          sizes[i][0] +
+          "<input type='radio' value='" + ourParlor.sizes[i].id + "'>" +
+          ourParlor.sizes[i].name + " $" + ourParlor.sizes[i].delta +
+        "</label>" +
+      "</div>"
+    );
+  }
+  for (var i = 0; i < ourParlor.toppings.length; i++) {
+    $("#toppings").append(
+      "<div class='checkbox'>" +
+        "<label>" +
+          "<input type='checkbox' value='" + ourParlor.toppings[i].id + "'>" +
+          ourParlor.toppings[i].name + " $" + ourParlor.toppings[i].delta +
         "</label>" +
       "</div>"
     );
@@ -32,35 +77,3 @@ $(function() {
 
   });
 });
-
-
-//SIzes: sm 10", md 12", lg 14",
-//CHOOSE TOPPINGS
-// CHOOSE MEATS
-//
-//  Pepperoni
-//  Sliced Italian Sausage
-//  Philly Steak
-//  Bacon
-//  Premium Chicken
-//  Italian Sausage
-//  Beef
-//  Ham
-//  Salami
-// CHOOSE NON-MEATS
-//
-//  Cheddar Cheese
-//  Shredded Parmesan Asiago
-//  Banana Peppers
-//  Green Peppers
-//  Mushrooms
-//  Onions
-//  Spinach
-//  Hot Sauce
-//  Feta Cheese
-//  Shredded Provolone Cheese
-//  Black Olives
-//  Jalapeno Peppers
-//  Pineapple
-//  Roasted Red Peppers
-//  Diced Tomatoes
